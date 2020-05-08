b9d0099d59e5da567a6ab15fc5cc007f1e807378
Ryan-Fuge <ryan.fugeworks@gmail.com>
2020-05-08 12:41:18 +0100

    Built Environment
    Added room, lighting, standard assets package, abstract toggle and sound

---
+++ Gallery/Assets/Scripts/PanelOpener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Panel;
+        void Start()
+    {
+        Panel.SetActive(false);
+    }
+    public void OpenPanel()
+
+    {
+        if (Panel != null)
+        {
+
+            bool isActive = Panel.activeInHierarchy;
+            Panel.SetActive(!isActive);
+        }
+
+
+    }
+}
